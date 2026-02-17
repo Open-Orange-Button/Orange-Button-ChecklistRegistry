0c5ad6ce55316be22993a416b695e48d9bfa6ea3
Taos Transue <spacechips@rocketmail.com>
2026-02-17 00:35:56 -0700

    add https cert

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -36,3 +36,17 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.app.arn
   }
 }
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08" # Standard AWS policy
+  #certificate_arn   = aws_acm_certificate.cert.arn
+  certificate_arn   = aws_acm_certificate_validation.example.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
