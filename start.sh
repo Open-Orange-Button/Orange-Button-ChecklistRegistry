b73624c9652c8ea651afaab64b4435344965ed11
Taos Transue <spacechips@rocketmail.com>
2026-02-16 23:28:49 -0700

    collectstatic

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 uv run python manage.py migrate --noinput
-# python manage.py collectstatic --noinput
+uv run python manage.py collectstatic --noinput
 uv run gunicorn checklist.wsgi:application --bind 0.0.0.0:8000 --log-level info
