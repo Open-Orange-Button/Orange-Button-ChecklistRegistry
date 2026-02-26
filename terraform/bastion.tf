f777e17d003962bb60fc567540c9206fdabe44d3
Taos Transue <spacechips@rocketmail.com>
2026-02-26 10:48:56 -0700

    update terraform to match the product registry

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -28,14 +28,14 @@ resource "aws_key_pair" "bastion_key" {
 
 # 3. Bastion EC2 Instance (Amazon Linux 2023)
 resource "aws_instance" "bastion" {
-  ami                         = "ami-0e2694e3cb74a4c35"
+  ami                         = "ami-02777684819ca2214"
   instance_type               = "t3.micro"
   subnet_id                   = module.vpc.public_subnets[0]
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   key_name                    = aws_key_pair.bastion_key.key_name
   associate_public_ip_address = true
 
-  tags = { Name = "Django-Bastion" }
+  tags = { Name = "${var.service-name}-Bastion" }
 }
 
 # Output the Public IP for connection
@@ -43,11 +43,11 @@ output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
 
-resource "aws_security_group_rule" "allow_bastion_to_rds" {
-  type                     = "ingress"
-  from_port                = 3306
-  to_port                  = 3306
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.rds_sg.id
-  source_security_group_id = aws_security_group.bastion_sg.id
-}
+# resource "aws_security_group_rule" "allow_bastion_to_rds" {
+#   type                     = "ingress"
+#   from_port                = 3306
+#   to_port                  = 3306
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.rds_sg.id
+#   source_security_group_id = aws_security_group.bastion_sg.id
+# }
